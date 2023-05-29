e0626cd94428c3ac1629a57c6173a7db2f68564a
ngocty05 <nb447105@gmail.com>
2023-05-29 09:09:59 +0700

    create model, sql

---
+++ MyUIGame/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Attribute)
+admin.site.register(Territory)
+admin.site.register(Figure)
+admin.site.register(Gender)
+admin.site.register(Weapone_type)
+admin.site.register(Weapone)
+admin.site.register(Figures_skill)
+admin.site.register(Figures_weapon)
+admin.site.register(Skill)
---
+++ MyUIGame/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyuigameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MyUIGame'
