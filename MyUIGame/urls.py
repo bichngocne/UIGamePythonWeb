61b9d79b1e0dcbe45459a91e96e85bb5c4879d82
ngocty05 <nb447105@gmail.com>
2023-05-31 17:10:29 +0700

    dispplay figure detail

--- MyUIGame/urls.py
+++ MyUIGame/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('index', views.index, name="index"),
     path('weapons', views.get_all_weapon, name='weapons'),
     path('figures', views.get_figure, name='figures'),
+    path('territories', views.get_territory, name='territories'),
+    path('figure/<int:id>', views.get_figure_detail, name='figuredetail'),
 ]
