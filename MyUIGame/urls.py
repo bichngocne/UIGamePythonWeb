4691c0cc96166cdbddd16ee72597a524c85d676c
ngocty05 <nb447105@gmail.com>
2023-05-31 10:06:00 +0700

    update list figure_territory, create list weapon

--- MyUIGame/urls.py
+++ MyUIGame/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('index', views.index, name="index"),
+    path('weapons', views.get_all_weapon, name='weapons'),
 ]
--- static/asset/js/app.js
+++
@@ -1,1 +0,0 @@
-const changeContent = document.querySelector('.content');
