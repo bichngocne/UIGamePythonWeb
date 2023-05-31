c763e796b2f4b65b7bb167248fed563eff8560d1
ngocty05 <nb447105@gmail.com>
2023-05-31 11:43:04 +0700

    Display attribute's figure

--- MyUIGame/views.py
+++ MyUIGame/views.py
@@ -3,6 +3,7 @@ from .models import Figure as figure_model
 from .models import Weapone as weapon_model
 from .models import Weapone_type as weapon_type_model
 from .models import Territory as territory_model
+from .models import Attribute as attribute_model
 # Create your views here.
 
 
@@ -16,3 +17,7 @@ def get_all_weapon(request):
     weapons = weapon_model.objects.select_related("weapone_type").all()
     weapon_types = weapon_type_model.objects.all()
     return render(request, 'weapons.html',{'weapons': weapons, 'weapon_types': weapon_types})
+def get_figure(request):
+    figure_list = figure_model.objects.select_related('attribute').all()
+    attributes = attribute_model.objects.all()
+    return render(request, 'figure.html', {'figures': figure_list, 'attributes': attributes})
